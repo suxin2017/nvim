d9ac454226b12fb541b73af21b4f2dc5238915b5
suxin2017 <1107178482@qq.com>
2024-04-06 19:41:48 +0800

    done

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,18 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    -- or                              , branch = '0.1.x',
-    dependencies = {'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep'}
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	-- or                              , branch = '0.1.x',
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"BurntSushi/ripgrep",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+  config = function ()
+    
+    local telescope = require "telescope"
+
+    telescope.setup({})
+
+    telescope.load_extension("fzf")
+  end
 }
